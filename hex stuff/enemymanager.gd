434d1b38191de7211d8a66a8a7d0d090c59dc0f3
soulmars432-ctrl <soulmars432@gmail.com>
2026-09-05 11:18:14 -0700

    dragon movement added, need to add corresponding informatics

--- hex stuff/enemymanager.gd
+++ hex stuff/enemymanager.gd
@@ -10,6 +10,9 @@ signal wave_started
 
 func _ready() -> void:
 	TurnManager.turn_started.connect(_on_turn_started)
+	print(Engine.get_version_info())
+	
+
 
 func _on_turn_started(state) -> void:
 	if state == TurnManager.State.Enemyturn:
@@ -51,9 +54,43 @@ func step_enemies_toward_player(player_cell: Vector2i, tilemap: TileMap) -> void
 		enemies[next_cell] = enemy
 
 	TurnManager.end_enemy_turn()
+	
+func step_dragon_toward_player(player_cell: Vector2i, tilemap: TileMap) -> void:
+	var old_positions = enemies.duplicate()
+	enemies.clear()
+
+	for cell in old_positions.keys():
+		var enemy = old_positions[cell]
+		var next_cell = _get_dragon_step_toward(cell, player_cell, tilemap)
+
+		if next_cell == player_cell:
+			GameManager.player_died()
+			enemies[cell] = enemy
+			continue
+
+		enemy.position = tilemap.map_to_local(next_cell)
+		enemies[next_cell] = enemy
+
+	TurnManager.end_enemy_turn()
 
 func _get_step_toward(from: Vector2i, to: Vector2i, tilemap: TileMap) -> Vector2i:
 	var neighbors = tilemap.get_surrounding_cells(from)
+	var best = from
+	var best_dist = _hex_distance(from, to)
+	for n in neighbors:
+		if enemies.has(n):
+			continue
+		if not tilemap.get_used_cells(0).has(n):
+			continue
+		var d = _hex_distance(n, to)
+		if d < best_dist:
+			best_dist = d
+			best = n
+	return best
+func _get_dragon_step_toward(from: Vector2i, to: Vector2i, tilemap: TileMap) -> Vector2i:
+	var test = tilemap.get_neighbor_cell(Vector2i(3,3), TileSet.CELL_NEIGHBOR_RIGHT_SIDE)
+	print("from ", Vector2i(3,3), " -> ", test)
+	var neighbors = get_dragon_moves(from, tilemap)
 	var best = from
 	var best_dist = _hex_distance(from, to)
 	for n in neighbors:
@@ -99,3 +136,25 @@ func spawn_pending_wave(tilemap: TileMap, parent: Node) -> void:
 		register_enemy(enemy, cell)
 	pending_spawn_cells.clear()
 	wave_started.emit()
+
+func get_dragon_moves(pos: Vector2i, tilemap: TileMap) -> Array:
+	var res = []
+	var directions = [
+		Vector2i(1, 0),   # E
+	Vector2i(1, -1),  # NE
+	Vector2i(0, -1),  # N
+	Vector2i(-1, 0),  # W
+	Vector2i(-1, 1),  # SW
+	Vector2i(0, 1),   # S
+	]
+	for d in directions:
+		res.append_array(generate(pos, d, tilemap, []))
+	return res
+
+func generate(cell: Vector2i, dir: Vector2i, tilemap: TileMap, res: Array) -> Array:
+	var new_cell = cell + dir
+	if tilemap.get_cell_source_id(0, new_cell) == -1:
+		return res
+	res.append(new_cell)
+	generate(new_cell, dir, tilemap, res)
+	return res
