350d97dd000f6f8f8911b672ae1bc2fe3beae7b8
wondertrash <benqlxie@gmail.com>
2026-09-05 18:47:13 -0700

    gold score pop

--- hex stuff/score.gd
+++ hex stuff/score.gd
@@ -6,3 +6,7 @@ func _ready() -> void:
 
 func _on_score_changed(new_score: int) -> void:
 	text = "%d" % new_score
+	modulate = Color(1, 0.9, 0.2)
+	var tween = create_tween()
+	scale = Vector2(1.3, 1.3)
+	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.2).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
